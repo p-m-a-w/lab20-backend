7259ec80f5556bc89c9397e0c476122ccef5637e
PMaw <peerapolth3327@hotmail.com>
2023-03-10 07:32:01 +0700

    implement tic-tac-toe

--- src/main/java/com/example/demo/Canvas.java
+++ src/main/java/com/example/demo/Canvas.java
@@ -3,19 +3,25 @@ package com.example.demo;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import java.util.Objects;
 
 @Component
 public class Canvas {
     @Getter
     private String[][] colorGrid;
     @Getter
-    private final int width = 64;
+    private final int width = 3;
     @Getter
-    private final int height = 64;
+    private final int height = 3;
+    @Getter
+    private String winner;
 
     public Canvas() {
         colorGrid = new String[height][width];
+        reset();
+    }
+
+    public void reset() {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 colorGrid[y][x] = "#ffffff";
@@ -23,13 +29,56 @@ public class Canvas {
         }
     }
 
+    private boolean sameColorTiles(int[] y, int[] x) {
+        assert x.length == y.length && x.length >= 2;
+        if (colorGrid[y[0]][x[0]].equals("#ffffff"))
+            return false;
+        for (int i = 1; i < x.length; i++) {
+            if (!Objects.equals(
+                    colorGrid[y[i]][x[i]],
+                    colorGrid[y[0]][x[0]]
+            ))
+                return false;
+        }
+        return true;
+    }
+
+    public String winner() {
+        for (int i = 0; i < 3; i++) {
+            if (sameColorTiles(new int[]{i, i, i}, new int[]{0, 1, 2}))
+                return colorGrid[i][0];
+            if (sameColorTiles(new int[]{0, 1, 2}, new int[]{i, i, i}))
+                return colorGrid[0][i];
+        }
+        if (
+                sameColorTiles(new int[]{0, 1, 2}, new int[]{0, 1, 2}) ||
+                        sameColorTiles(new int[]{2, 1, 0}, new int[]{0, 1, 2})
+        )
+            return colorGrid[1][1];
+
+        int size = 0;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (!Objects.equals(colorGrid[y][x], "#ffffff"))
+                    size++;
+            }
+        }
+        return size >= 9 ? "over" : null;
+    }
+
+    public boolean available(PaintMessage paintMessage) {
+        int posX = paintMessage.getPosX();
+        int posY = paintMessage.getPosY();
+        return Objects.equals(colorGrid[posY][posX], "#ffffff");
+    }
+
     public Canvas paint(PaintMessage paintMessage) {
         int posX = paintMessage.getPosX();
         int posY = paintMessage.getPosY();
-        if (posX >= 0 && posX < width && posY >= 0 && posY < height) {
+        if (posX >= 0 && posX < width && posY >= 0 && posY < height)
             colorGrid[posY][posX] = paintMessage.getColor();
-        }
-        System.out.println(Arrays.deepToString(colorGrid));
+        winner = winner();
+        if (winner != null) reset();
         return this;
     }
 }
--- src/main/java/com/example/demo/CanvasController.java
+++ src/main/java/com/example/demo/CanvasController.java
@@ -3,21 +3,25 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class CanvasController {
-    @Autowired
-    private SimpMessagingTemplate template;
-
+    private String turn;
     @Autowired
     private Canvas canvas;
 
     @MessageMapping("/paint")
     @SendTo("/topic/canvas")
     public Canvas paint(PaintMessage paintMessage) {
+        if (canvas.getWinner() != null) {
+            canvas.reset();
+            turn = null;
+        }
+        if (paintMessage.getColor().equals(turn) || !canvas.available(paintMessage))
+            return canvas;
+        turn = paintMessage.getColor();
         return canvas.paint(paintMessage);
     }
 
